149a041309fb24730e429d3b5fc505e01c2b44a7
Andrew Stratmann <ads5743@truman.edu>
2017-04-04 00:06:14 -0500

    compliance with standard js

--- freeCodeCamp Projects/JS Calculator/js/index.js
+++ freeCodeCamp Projects/JS Calculator/js/index.js
@@ -1,190 +1,181 @@
-$(document).ready(function(){
-  var startVal = ""; //nothing there
-  var curString = startVal;
-  $("#dispval").html("0");
-  
-  $("#acbut").click(function(e){
-    e.preventDefault();
-    curString = startVal;
-    $("#dispval").html("0");
-  });
-  
-  $("#cebut").click(function(e){
-    e.preventDefault();
-    curString = curString.slice(0, -1);
-    if(curString.length > 0){
-      $("#dispval").html(curString);
+$(document).ready(function () {
+  var startVal = '' // nothing there
+  var curString = startVal
+  $('#dispval').html('0')
+
+  $('#acbut').click(function (e) {
+    e.preventDefault()
+    curString = startVal
+    $('#dispval').html('0')
+  })
+
+  $('#cebut').click(function (e) {
+    e.preventDefault()
+    curString = curString.slice(0, -1)
+    if (curString.length > 0) {
+      $('#dispval').html(curString)
+    } else {
+      $('#dispval').html('0')
     }
-    else{
-      $("#dispval").html("0");
+  })
+
+  $('#7but').click(function (e) {
+    e.preventDefault()
+    curString += '7'
+    $('#dispval').html(curString)
+  })
+
+  $('#8but').click(function (e) {
+    e.preventDefault()
+    curString += '8'
+    $('#dispval').html(curString)
+  })
+
+  $('#9but').click(function (e) {
+    e.preventDefault()
+    curString += '9'
+    $('#dispval').html(curString)
+  })
+
+  $('#divbut').click(function (e) {
+    e.preventDefault()
+    if ($.isNumeric(curString.slice(-1))) {
+      curString += '/'
     }
-  });
-  
-  $("#7but").click(function(e){
-    e.preventDefault();
-    curString += "7";
-    $("#dispval").html(curString);
-  });
-  
-  $("#8but").click(function(e){
-    e.preventDefault();
-    curString += "8";
-    $("#dispval").html(curString);
-  });
-  
-  $("#9but").click(function(e){
-    e.preventDefault();
-    curString += "9";
-    $("#dispval").html(curString);
-  });
-  
-  $("#divbut").click(function(e){
-    e.preventDefault();
-    if($.isNumeric(curString.slice(-1))){
-      curString += "/";
+    $('#dispval').html(curString)
+  })
+
+  $('#mulbut').click(function (e) {
+    e.preventDefault()
+    if ($.isNumeric(curString.slice(-1))) {
+      curString += '*'
     }
-    $("#dispval").html(curString);
-  });
-  
-  $("#mulbut").click(function(e){
-    e.preventDefault();
-    if($.isNumeric(curString.slice(-1))){
-      curString += "*";
+    $('#dispval').html(curString)
+  })
+
+  $('#minbut').click(function (e) {
+    e.preventDefault()
+    if ($.isNumeric(curString.slice(-1))) {
+      curString += '-'
     }
-    $("#dispval").html(curString);
-  });
-  
-  $("#minbut").click(function(e){
-    e.preventDefault();
-    if($.isNumeric(curString.slice(-1))){
-      curString += "-";
+    $('#dispval').html(curString)
+  })
+
+  $('#plusbut').click(function (e) {
+    e.preventDefault()
+    if ($.isNumeric(curString.slice(-1))) {
+      curString += '+'
     }
-    $("#dispval").html(curString);
-  });
-  
-  $("#plusbut").click(function(e){
-    e.preventDefault();
-    if($.isNumeric(curString.slice(-1))){
-      curString += "+";
-    }
-    $("#dispval").html(curString);
-  });
-  
-  $("#4but").click(function(e){
-    e.preventDefault();
-    curString += "4";
-    $("#dispval").html(curString);
-  });
-  
-  $("#5but").click(function(e){
-    e.preventDefault();
-    curString += "5";
-    $("#dispval").html(curString);
-  });
-  
-  $("#6but").click(function(e){
-    e.preventDefault();
-    curString += "6";
-    $("#dispval").html(curString);
-  });
-  
-  $("#1but").click(function(e){
-    e.preventDefault();
-    curString += "1";
-    $("#dispval").html(curString);
-  });
-  
-  $("#2but").click(function(e){
-    e.preventDefault();
-    curString += "2";
-    $("#dispval").html(curString);
-  });
-  
-  $("#3but").click(function(e){
-    e.preventDefault();
-    curString += "3";
-    $("#dispval").html(curString);
-  });
-  
-  $("#0but").click(function(e){
-    e.preventDefault();
-    curString += "0";
-    $("#dispval").html(curString);
-  });
-  
-  $("#dotbut").click(function(e){
-    e.preventDefault();
-    curString += ".";
-    $("#dispval").html(curString);
-  });
-  
-  $("#eqbut").click(function(e){
-    e.preventDefault();
-    //curstring contains the input
-    var i = 0;
-    var args = [];
-    var current = "";
-    while(i < curString.length){
-      if(curString[i] == '/' || curString[i] == '*' || curString[i] == '+' || curString[i] == '-'){
-        //a symbol is found
-        args.push(current);
-        current = "";
-        args.push(curString[i]);
+    $('#dispval').html(curString)
+  })
+
+  $('#4but').click(function (e) {
+    e.preventDefault()
+    curString += '4'
+    $('#dispval').html(curString)
+  })
+
+  $('#5but').click(function (e) {
+    e.preventDefault()
+    curString += '5'
+    $('#dispval').html(curString)
+  })
+
+  $('#6but').click(function (e) {
+    e.preventDefault()
+    curString += '6'
+    $('#dispval').html(curString)
+  })
+
+  $('#1but').click(function (e) {
+    e.preventDefault()
+    curString += '1'
+    $('#dispval').html(curString)
+  })
+
+  $('#2but').click(function (e) {
+    e.preventDefault()
+    curString += '2'
+    $('#dispval').html(curString)
+  })
+
+  $('#3but').click(function (e) {
+    e.preventDefault()
+    curString += '3'
+    $('#dispval').html(curString)
+  })
+
+  $('#0but').click(function (e) {
+    e.preventDefault()
+    curString += '0'
+    $('#dispval').html(curString)
+  })
+
+  $('#dotbut').click(function (e) {
+    e.preventDefault()
+    curString += '.'
+    $('#dispval').html(curString)
+  })
+
+  $('#eqbut').click(function (e) {
+    e.preventDefault()
+    // curstring contains the input
+    var i = 0
+    var args = []
+    var current = ''
+    while (i < curString.length) {
+      if (curString[i] == '/' || curString[i] == '*' || curString[i] == '+' || curString[i] == '-') {
+        // a symbol is found
+        args.push(current)
+        current = ''
+        args.push(curString[i])
+      } else {
+        current += curString[i]
       }
-      else{
-        current += curString[i];
+
+      if (i == curString.length - 1) {
+        args.push(current)
       }
-      
-      if(i == curString.length - 1){
-        args.push(current);
-      }
-      i++;
+      i++
     }
-    
-    for(var i = 0; i < args.length; i++){
-      if($.isNumeric(args[i])){
-        if(args[i].indexOf('.') >= 0){
-          args[i] = parseFloat(args[i]);
-        }else{
-          args[i] = parseInt(args[i]);
+
+    for (var i = 0; i < args.length; i++) {
+      if ($.isNumeric(args[i])) {
+        if (args[i].indexOf('.') >= 0) {
+          args[i] = parseFloat(args[i])
+        } else {
+          args[i] = parseInt(args[i])
         }
       }
     }
-    
-    
-    //work on error handling
-    var value = args[0];
-    var counter = 1;
-    while(counter < args.length){
-      if($.isNumeric(args[counter])){
-        //error
-        //the preceeding token was a num
-        curString = "";
-        $("#dispval").html("err");
-      }
-      else if(args[counter] == '*'){
-        value = value * args[counter + 1];
-        counter += 2;
-      }
-      else if(args[counter] == '/'){
-        value = value / args[counter + 1];
-        counter += 2;
-      }
-      else if(args[counter] == '-'){
-        value = value - args[counter + 1];
-        counter += 2;
-      }
-      else if(args[counter] == '+'){
-        value = value + args[counter + 1];
-        counter += 2;
+
+    // work on error handling
+    var value = args[0]
+    var counter = 1
+    while (counter < args.length) {
+      if ($.isNumeric(args[counter])) {
+        // error
+        // the preceeding token was a num
+        curString = ''
+        $('#dispval').html('err')
+      } else if (args[counter] == '*') {
+        value = value * args[counter + 1]
+        counter += 2
+      } else if (args[counter] == '/') {
+        value = value / args[counter + 1]
+        counter += 2
+      } else if (args[counter] == '-') {
+        value = value - args[counter + 1]
+        counter += 2
+      } else if (args[counter] == '+') {
+        value = value + args[counter + 1]
+        counter += 2
       }
     }
-    
-    value = Math.ceil(100 * value) / 100;
-    
-    curString = value.toString();
-    $("#dispval").html(curString);
-    
-    
-  });
-});
+
+    value = Math.ceil(100 * value) / 100
+
+    curString = value.toString()
+    $('#dispval').html(curString)
+  })
+})
--- freeCodeCamp Projects/Wikipedia Viewer/js/index.js
+++ freeCodeCamp Projects/Wikipedia Viewer/js/index.js
@@ -1,58 +1,52 @@
-$(document).ready(function(){
-  
-  //what to do in the event of a button press
-  $("#subbut").click(function(e){
-    e.preventDefault();
-    var query = $("#quer").val();
-    //alert(query);
-    var jsonq = "https://en.wikipedia.org/w/api.php?action=opensearch&search=";
-    jsonq += query;
-    jsonq += "&limit=10&namespace=0&format=json&origin=*";
-    
-    $.getJSON(jsonq, function(result){
-      //$("#results").html(JSON.stringify(result));
-      
-      //first get the data from the query
-      var numOfResults = result[1].length;
-      
-      var qtitles = [];
-      for(var t = 0; t < 10; t++)
-      {
-        qtitles[t] = result[1][t];
+$(document).ready(function () {
+  // what to do in the event of a button press
+  $('#subbut').click(function (e) {
+    e.preventDefault()
+    var query = $('#quer').val()
+    // alert(query);
+    var jsonq = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='
+    jsonq += query
+    jsonq += '&limit=10&namespace=0&format=json&origin=*'
+
+    $.getJSON(jsonq, function (result) {
+      // $("#results").html(JSON.stringify(result));
+
+      // first get the data from the query
+      var numOfResults = result[1].length
+
+      var qtitles = []
+      for (var t = 0; t < 10; t++) {
+        qtitles[t] = result[1][t]
       }
-      
-      var qdescrips = [];
-      for(var u = 0; u < 10; u++)
-      {
-        qdescrips[u] = result[2][u];
+
+      var qdescrips = []
+      for (var u = 0; u < 10; u++) {
+        qdescrips[u] = result[2][u]
       }
-      
-      var qlinks = [];
-      for(var v = 0; v < 10; v++)
-      {
-        qlinks[v] = result[3][v];
+
+      var qlinks = []
+      for (var v = 0; v < 10; v++) {
+        qlinks[v] = result[3][v]
       }
-      
-      //first clear all of the results
-      
-      var titles = ["#firstt", "#secondt", "#thirdt", "#fourtht", "#fiftht", "#sixtht", "#seventht", "#eightht", "#nintht", "#tentht"];
-      
-      var descrips = ["#firstdesc", "#seconddesc", "#thirddesc", "#fourthdesc", "#fifthdesc", "#sixthdesc", "#seventhdesc", "#eighthdesc", "#ninthdesc", "#tenthdesc"];
-      
-      var links = ["#firstlink", "#secondlink", "#thirdlink", "#fourthlink", "#fifthlink", "#sixthlink", "#seventhlink", "#eighthlink", "#ninthlink", "#tenthlink"];
-      
-      for(var i = 0; i < descrips.length; i++)
-      {
-        $(titles[i], descrips[i]).html("");
-        $(links[i]).attr('href', "");
+
+      // first clear all of the results
+
+      var titles = ['#firstt', '#secondt', '#thirdt', '#fourtht', '#fiftht', '#sixtht', '#seventht', '#eightht', '#nintht', '#tentht']
+
+      var descrips = ['#firstdesc', '#seconddesc', '#thirddesc', '#fourthdesc', '#fifthdesc', '#sixthdesc', '#seventhdesc', '#eighthdesc', '#ninthdesc', '#tenthdesc']
+
+      var links = ['#firstlink', '#secondlink', '#thirdlink', '#fourthlink', '#fifthlink', '#sixthlink', '#seventhlink', '#eighthlink', '#ninthlink', '#tenthlink']
+
+      for (var i = 0; i < descrips.length; i++) {
+        $(titles[i], descrips[i]).html('')
+        $(links[i]).attr('href', '')
       }
-      
-      for(var j = 0; j < numOfResults; j++)
-      {
-        $(titles[j]).html(qtitles[j]);
-        $(descrips[j]).html(qdescrips[j] + "<hr>");
-        $(links[j]).attr('href', qlinks[j]);
+
+      for (var j = 0; j < numOfResults; j++) {
+        $(titles[j]).html(qtitles[j])
+        $(descrips[j]).html(qdescrips[j] + '<hr>')
+        $(links[j]).attr('href', qlinks[j])
       }
-    });
-  });
-});
+    })
+  })
+})
